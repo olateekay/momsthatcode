7852433758eb3f06e2bb3ea50156e9e237385f22
Olatokunbo <Olatokunbo@Javas-MacBook-Pro.local>
2020-03-14 01:31:03 +0100

    Stared with styling using styled components

--- App.js
+++ App.js
@@ -1,19 +1,46 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {Text} from 'react-native';
+import styled from 'styled-components'
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Moms that code!</Text>
-    </View>
+    <Container>
+    <TitleBar>
+    <Avatar source={require('./assets/teekay.jpg')}/>
+      <Title>Welcome back</Title>
+      <Name>Olatokunbo</Name>
+      </TitleBar>
+    </Container>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+ const Container = styled.View`
+   flex: 1;
+   background-color: #f0f3f5;
+   `;
+
+ const Title = styled.Text`
+   font-size: 16px;
+   color:#b8bece;
+   font-weight:500;
+ `;
+
+ const Name = styled.Text`
+   font-size: 20px;
+   color: #3c4560;
+   font-weight: bold;
+ `;
+
+ const TitleBar = styled.View`
+   width:100%;
+   margin-Top:50px;
+   padding-left:20px;
+ `;
+
+ const Avatar = styled.Image`
+   width: 44px;
+   height:44px;
+   background: black;
+   border-radius: 22px;
+   margin-left: 20px;
+ `;
